bbcfe242a9a16d425e437c1c2bb92e4ebf9d6fef
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-04-20 17:54:23 +0600

    add a all booking info with json

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Hotel-Booking
+    // all-booking-info
+
+    const hotelBookCollection = client.db("Hotel-Booking").collection("all-booking-info")
+    app.get('/all-booking-info', async (req, res) => {
+      const result = await hotelBookCollection.find().toArray();
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
