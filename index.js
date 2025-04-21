da2bb4ce097fbada08844c459356646179d01ffd
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-04-21 22:51:48 +0600

    cors disable

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
-app.use(cors())
+// app.use(cors())
 app.use(express.json());
 const port = process.env.port || 5000
 
